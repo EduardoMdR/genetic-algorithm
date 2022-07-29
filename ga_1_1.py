9c53bbbe12b392de55b03f1bc6640bf8f3e1bf32
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2022-07-28 22:42:52 -0300

    :sparkles: Inicializando ga1.1

---
+++ ga_1_1.py
@@ -0,0 +1,12 @@
+# Inicio do algoritmo genético
+
+# Variáveis globais estáticas
+cromossomos = 44
+populacao = 100
+geracoes = 40
+taxa_mut = 0.008
+taxa_cro = 0.65
+
+# def main(args):
+print("Algoritmo genético 1-1")
+
