bff5c9eea76e53f4bfb0e246b916c4f45c9fe951
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2022-07-28 22:57:23 -0300

    :sparkles: Desenvolvendo resolução
    Construindo um algoritmo para resolução do GA

--- ga_1_1.py
+++ ga_1_1.py
@@ -1,5 +1,17 @@
 # Inicio do algoritmo genético
 
+# ideia de implementação
+# Passo 1: Crio uma população inicial randômica (100)
+# Passo 2: verifico o fitness de cada indivíduo
+# Passo 3: Seleciono dois pais (roleta) e gero dois filhos
+# Passo 4: Aplico crossover (se taxa_cro = verdade)
+# Passo 5: Aplico mutação (se taxa_mut = verdade)
+# Passo 6: Repito esse processo até completar todas as gerações
+# Fim: vou ter o indivíduo melhor qualificado como minha resposta
+
+# bibliotecas
+import numpy as np
+
 # Variáveis globais estáticas
 cromossomos = 44
 populacao = 100
@@ -10,3 +22,9 @@ taxa_cro = 0.65
 # def main(args):
 print("Algoritmo genético 1-1")
 
+# Passo 1:
+geracao_atual = [populacao]
+
+for filho in range(populacao):
+  # geracao_atual[filho] = np.random.randint(2, size=cromossomos)
+  # print(geracao_atual[filho])
