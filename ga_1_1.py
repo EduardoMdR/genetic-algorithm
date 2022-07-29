b41f515637f4c9b707b2c138532609d04ba4a5f0
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2022-07-28 23:24:43 -0300

    :sparkles: Implementando passo 1

--- ga_1_1.py
+++ ga_1_1.py
@@ -14,17 +14,22 @@ import numpy as np
 
 # Variáveis globais estáticas
 cromossomos = 44
-populacao = 100
-geracoes = 40
+populacao = 10      # 100
+geracoes = 4        # 40
 taxa_mut = 0.008
 taxa_cro = 0.65
+verbose = 0         # variavel utizada para debugar
+constante_normalizacao = 0.0000476837278899989
 
 # def main(args):
 print("Algoritmo genético 1-1")
 
 # Passo 1:
-geracao_atual = [populacao]
-
+geracao_atual = []
 for filho in range(populacao):
-  # geracao_atual[filho] = np.random.randint(2, size=cromossomos)
-  # print(geracao_atual[filho])
+  geracao_atual.append(np.random.randint(2, size=cromossomos))
+
+if(verbose):
+  print(geracao_atual)
+
+
