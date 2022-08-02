6f80d5b0e89b44b5fd1ba9b7ef69845a5d3a76ac
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2022-08-02 05:28:37 -0300

    :sparkles: Criando uma nova geração
    O passo 5 foi finalizado nesse Commit

--- ga_1_1.py
+++ ga_1_1.py
@@ -8,18 +8,18 @@
 # Passo 5: Aplico mutação (se taxa_mut = verdade)
 #          Faço isso 50 vezes para completar minha população
 # Passo 6: Repito esse processo até completar todas as gerações
+# Passo 7: Passar o resultado para um arquivo .txt
 # Fim: vou ter o indivíduo melhor qualificado (da última geração) como minha resposta
 
 
 # bibliotecas
-from audioop import cross
 import numpy as np
-import math
+# import math
 
 
 # Variáveis globais estáticas
 cromossomos = 44
-populacao = 5       # 100
+populacao = 6       # 100
 geracoes = 4        # 40
 taxa_mut = 0.008
 taxa_cro = 0.65
@@ -53,6 +53,7 @@ print("Algoritmo genético 1-1")
 
 # Passo 1:
 geracao_atual = []
+nova_geracao = []
 for filho in range(populacao):
   geracao_atual.append(np.random.randint(2, size=cromossomos))
 
@@ -79,42 +80,49 @@ if(verbose):
   print("Roleta: ", roleta)
 
 
-# Passo 3:
-pai_x = escolheFilho(np.random.randint(low=1, high=roleta), populacao, fitness)
-pai_y = escolheFilho(np.random.randint(low=1, high=roleta), populacao, fitness)
+for i in range(int(populacao/2)):
+  if(verbose): print(i+1,'º pais a serem escolhidos')
+  # pai_x = []
+  # Passo 3:
+  pai_x = escolheFilho(np.random.randint(low=1, high=roleta), populacao, fitness)
+  pai_y = escolheFilho(np.random.randint(low=1, high=roleta), populacao, fitness)
 
-if(verbose):
-  print('Pai X escolhido: ', pai_x)
-  print('Pai Y escolhido: ', pai_y)
-
-
-# Passo 4 (Preciso melhorar)
-# posso utilizar um randomizador para determinar o ponto de crossover
-crossover = np.random.randint(100)
-copia_pai_x, copia_pai_y = [], []
-
-if(crossover <= (taxa_cro*100)):
-  # print('Aconteceu crossover')
-  copia_pai_x = np.array_split(pai_x,2)
-  copia_pai_y = np.array_split(pai_y,2)
-
-  pai_x = np.concatenate((copia_pai_x[0], copia_pai_y[1]), axis=None)
-  pai_y = np.concatenate((copia_pai_x[1], copia_pai_y[0]), axis=None)
-
-if(verbose):
-  if(crossover < (taxa_cro*100)): print('Houve crossover')
-  print('Pai X pós crossover: ', pai_x)
-  print('Pai Y pós crossover: ', pai_y)
+  if(verbose):
+    print('Pai X escolhido: ', pai_x)
+    print('Pai Y escolhido: ', pai_y)
 
 
-# Passo 5
-pai_x = mutacaoGene(pai_x, taxa_mut, cromossomos)
-pai_y = mutacaoGene(pai_y, taxa_mut, cromossomos)
+  # Passo 4 (Preciso melhorar)
+  # posso utilizar um randomizador para determinar o ponto de crossover
+  crossover = np.random.randint(100)
+  copia_pai_x, copia_pai_y = [], []
+
+  if(crossover <= (taxa_cro*100)):
+    # print('Aconteceu crossover')
+    copia_pai_x = np.array_split(pai_x,2)
+    copia_pai_y = np.array_split(pai_y,2)
+
+    pai_x = np.concatenate((copia_pai_x[0], copia_pai_y[1]), axis=None)
+    pai_y = np.concatenate((copia_pai_x[1], copia_pai_y[0]), axis=None)
+
+  if(verbose):
+    if(crossover < (taxa_cro*100)): print('Houve crossover')
+    print('Pai X pós crossover: ', pai_x)
+    print('Pai Y pós crossover: ', pai_y)
 
 
-if(verbose):
-  print('pai_x final: ', pai_x)
-  print('pai_y final: ', pai_y)
+  # Passo 5
+  pai_x = mutacaoGene(pai_x, taxa_mut, cromossomos)
+  pai_y = mutacaoGene(pai_y, taxa_mut, cromossomos)
 
+
+  if(verbose):
+    print('pai_x final: ', pai_x)
+    print('pai_y final: ', pai_y)
+
+  nova_geracao.append(pai_x)
+  nova_geracao.append(pai_y)
+
+if(verbose): print(nova_geracao)
 
 # Realizar o procedimento anterior len(populacao) vezes
