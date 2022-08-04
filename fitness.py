8cfa0b522182e01cf1d791ac6c8c8100daa6a8df
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2022-08-04 13:35:35 -0300

    :sparkles: Desenvolvendo Fitness

--- fitness.py
+++ fitness.py
@@ -1,14 +1,13 @@
 import numpy as np
+import math
 
 populacao = 4
 cromossomos = 44
-constante_normalizacao = 0.0000476837278899989
+# constante_normalizacao = 0.0000476837278899989
+constante_normalizacao = (200 / (math.pow(2,22) - 1))
 geracao_atual = []
 
 geracao_atual.append(np.random.randint(2, size=cromossomos))
-
-roleta = 0
-fitness = []
 aux = ''
 x, y = '', ''
 
@@ -20,12 +19,23 @@ for i in (geracao_atual[0]):
   else: y += str(i)
   idx += 1
 
-print('Valor ed X: ', x)
-print('Valor ed Y: ',y)
+print('Valor de X: ', x)
+print('Valor de Y: ', y)
 
 # print(aux)
 x = (int(x, 2) * constante_normalizacao) - 100
 y = (int(y, 2) * constante_normalizacao) - 100
 
-print('Valor ed X: ', x)
-print('Valor ed Y: ',y)
+print('Valor de X: ', x)
+print('Valor de Y: ', y)
+
+eq_aux, eq_aux2 = 0, 0
+eq_aux = math.pow(math.pow(x,2) + math.pow(y,2), 0.5)
+eq_aux = math.pow(math.sin(eq_aux),2)
+eq_aux2 = 1.0 + (0.0001 * (math.pow(math.pow(x,2)+ math.pow(y,2),2)))
+
+print("Fitness final", 1 - (eq_aux/eq_aux2))
+
+# Exemplo
+# x = 0000101000011000000001
+# y = 1000101010001110111011
