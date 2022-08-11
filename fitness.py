0d391ea75d73e6bdfc13e1f6d7618cdd3dbba740
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2022-08-10 23:21:34 -0300

    Fitness correto

--- fitness.py
+++ fitness.py
@@ -3,12 +3,11 @@ import math
 
 populacao = 4
 cromossomos = 44
-# constante_normalizacao = 0.0000476837278899989
-constante_normalizacao = (200 / (math.pow(2,22) - 1))
+constante_normalizacao = 0.0000476837278899989
+# constante_normalizacao = (200 / (math.pow(2,22) - 1))
 geracao_atual = []
 
 geracao_atual.append(np.random.randint(2, size=cromossomos))
-aux = ''
 x, y = '', ''
 
 print(geracao_atual)
@@ -22,20 +21,15 @@ for i in (geracao_atual[0]):
 print('Valor de X: ', x)
 print('Valor de Y: ', y)
 
-# print(aux)
 x = (int(x, 2) * constante_normalizacao) - 100
 y = (int(y, 2) * constante_normalizacao) - 100
 
-print('Valor de X: ', x)
-print('Valor de Y: ', y)
+print('Valor de X normalizado: ', x)
+print('Valor de Y normalizado: ', y)
 
 eq_aux, eq_aux2 = 0, 0
-eq_aux = math.pow(math.pow(x,2) + math.pow(y,2), 0.5)
-eq_aux = math.pow(math.sin(eq_aux),2)
-eq_aux2 = 1.0 + (0.0001 * (math.pow(math.pow(x,2)+ math.pow(y,2),2)))
+eq_aux = math.pow((math.pow(x,2) + math.pow(y,2)), 0.5)
+eq_aux = (math.pow(math.sin(eq_aux),2))
+eq_aux2 = 1.0 + (0.0001 * (math.pow((math.pow(x,2) + math.pow(y,2)),2)))
 
-print("Fitness final", 1 - (eq_aux/eq_aux2))
-
-# Exemplo
-# x = 0000101000011000000001
-# y = 1000101010001110111011
+print("Fitness final", (1 - (eq_aux/eq_aux2)))
